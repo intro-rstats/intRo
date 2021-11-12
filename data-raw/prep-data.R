0bebfbb4b2c8f5f51b3cffd8c4bc7d794b4b4f5a
stefanocoretta <stefano.coretta@gmail.com>
2021-11-12 17:38:29 +0000

    ✨ - add harry data

--- data-raw/prep-data.R
+++ data-raw/prep-data.R
@@ -11,12 +11,8 @@ endangered <- read_csv("data-raw/endangered_lang.csv")
 colnames(endangered) <- c("id", "name", "name_fr", "name_es", "countries", "country_code", "iso", "degree", "alternate", "endonym", "speakers", "sources", "latitude", "longitude", "location")
 usethis::use_data(endangered)
 
-harry_potter <- tibble(
-  house = c(
-    rep("Gryffindor", 90),
-    rep("Slytherin", 134),
-    rep("Hufflepuff", 91),
-    rep("Ravenclaw", 130)
-  )
-)
+harry_potter <- read_csv("data-raw/harry_potter.csv")
 usethis::use_data(harry_potter)
+
+source("https://gist.githubusercontent.com/z3tt/301bb0c7e3565111770121af2bd60c11/raw/ae538a28f2701f261a9ccf4ad20377e1b345fdab/data-ggplot-evolution-vol1.r")
+usethis::use_data(df_ratios)
