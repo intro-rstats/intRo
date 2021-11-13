597397bf57efb0abda4eabbe68e357b89b270c8e
stefanocoretta <stefano.coretta@gmail.com>
2021-11-13 15:51:34 +0000

    ♻️ - add data

--- data-raw/prep-data.R
+++ data-raw/prep-data.R
@@ -7,8 +7,19 @@ social <- social %>%
   mutate(users = users / 1000000)
 usethis::use_data(social)
 
-endangered <- read_csv("data-raw/endangered_lang.csv")
-colnames(endangered) <- c("id", "name", "name_fr", "name_es", "countries", "country_code", "iso", "degree", "alternate", "endonym", "speakers", "sources", "latitude", "longitude", "location")
+endangered <- read_csv("https://raw.githubusercontent.com/stefanocoretta/glottolog-cldf/master/cldf/values.csv") %>%
+  filter(Parameter_ID == "aes") %>%
+  mutate(
+    status = case_when(
+      Value == "<NA>" ~ "unknown",
+      Value == "1" ~ "not endangered",
+      Value == "2" ~ "threatened",
+      Value == "3" ~ "shifting",
+      Value == "4" ~ "moribund",
+      Value == "5" ~ "nearly extinct",
+      Value == "6" ~ "extinct"
+    )
+  )
 usethis::use_data(endangered)
 
 harry_potter <- read_csv("data-raw/harry_potter.csv")
@@ -16,3 +27,9 @@ usethis::use_data(harry_potter)
 
 source("https://gist.githubusercontent.com/z3tt/301bb0c7e3565111770121af2bd60c11/raw/ae538a28f2701f261a9ccf4ad20377e1b345fdab/data-ggplot-evolution-vol1.r")
 usethis::use_data(df_ratios)
+
+slavery_tot <- read_csv("data-raw/Trans-Atlantic_Slave_Trade_totals.csv")
+usethis::use_data(slavery_tot)
+
+pyramids <- read_csv("data-raw/pyramids.csv")
+usethis::use_data(pyramids)
