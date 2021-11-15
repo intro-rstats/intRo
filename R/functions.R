893b206062657dd1371d108689137fb9ca442893
stefanocoretta <stefano.coretta@gmail.com>
2021-11-15 17:22:23 +0000

    🐛 - fix dupl chunk; update function

--- R/functions.R
+++ R/functions.R
@@ -9,19 +9,36 @@
 #'
 #' @examples
 #' # run_tutorial()
-run_tutorial <- function() {
+intro_tutorial <- function() {
   num <- utils::menu(
-    tutorials,
+    sessions,
     title = "Which tutorial shall I run?"
   )
 
-  learnr::run_tutorial(tutorials[num], package = "intRo")
+  if (num %in% c(1, 5)) {
+    cat("Just kidding! There is no tutorial for this session. Try a different one.")
+  } else {
+    learnr::run_tutorial(tutorials[num], package = "intRo")
+  }
+
 }
 
 tutorials <- c(
+  "01_introduction",
   "02_r_basics",
   "03_plotting",
   "04_import_data",
+  "05_research_manage",
   "06_tidy",
   "07_advanced"
+)
+
+sessions <- c(
+  "01 - Introduction",
+  "02 - R basics",
+  "03 - Plot with ggplot2",
+  "04 - Import and transform data",
+  "05 - Research data analysis",
+  "06 - Tidy data",
+  "07 - Advanced skills"
 )
